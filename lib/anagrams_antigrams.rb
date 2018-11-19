a73107aab13ec966868b878bbfe91c63bdf0df39
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-11-19 09:05:35 -0800

    Pass test 4 to check for antigram

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 class Words
-  
+
   def anagram(word_one, word_two)
     word_one_array = word_one.downcase.split("")
     word_two_array = word_two.downcase.split("")
@@ -10,8 +10,10 @@ class Words
     if word?(word_one_array) & word?(word_two_array)
       if array_compare == word_one_array
         "#{word_one} and #{word_two} are anagrams"
+      elsif array_compare.length == 0
+        "#{word_one} and #{word_two} have no letter matches and are antigrams"
       else
-        "#{word_one} and #{word_two} are not anagrams"
+        "#{word_one} and #{word_two} are not anagrams or antigrams"
       end
     else
       'You need to input actual words!'
