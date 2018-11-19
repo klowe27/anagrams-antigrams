a17bc521a80e950dddd33986d72eb80cc9e33404
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-11-19 07:41:25 -0800

    Add test 1; fails

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -1,1 +1,7 @@
 require 'pry'
+
+class Words
+  def anagrams(word_one, word_two)
+    # code to go here
+  end
+end
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -1,2 +1,9 @@
 require 'rspec'
 require 'anagrams_antigrams'
+
+describe('Words#anagrams') do
+  it('will compare two words and return whether they are anagrams') do
+    words = Words.new()
+    expect(words.anagrams('ruby', 'bury')).to(eq('ruby and bury are anagrams'))
+  end
+end
