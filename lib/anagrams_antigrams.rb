3356e00f278ac6f8aa11d3581f89ae933e79c4c2
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-11-19 09:00:30 -0800

    Pass test 3 to check for words

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -1,21 +1,27 @@
 require 'pry'
 
 class Words
+  
   def anagram(word_one, word_two)
     word_one_array = word_one.downcase.split("")
     word_two_array = word_two.downcase.split("")
     array_compare = word_one_array & word_two_array
 
-    if array_compare == word_one_array
-      "#{word_one} and #{word_two} are anagrams"
+    if word?(word_one_array) & word?(word_two_array)
+      if array_compare == word_one_array
+        "#{word_one} and #{word_two} are anagrams"
+      else
+        "#{word_one} and #{word_two} are not anagrams"
+      end
     else
-      "#{word_one} and #{word_two} are not anagrams"
+      'You need to input actual words!'
     end
-
   end
 
-  def word? (word_one_array, word_two_array)
+  def word? (word_array)
     vowels = ["a","e","i","o", "u", "y"]
+    vowels_in_word = vowels & word_array
+    vowels_in_word.length > 0
   end
 
 end
