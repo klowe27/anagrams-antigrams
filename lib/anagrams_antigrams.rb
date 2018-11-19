462eb749738d6f75001cc2a14142f9b70b8cfc37
Kristin Brewer-Lowe <kristin.lowe1@gmail.com>
2018-11-19 07:58:22 -0800

    Pass test 1 to check for anagram

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -1,7 +1,14 @@
 require 'pry'
 
 class Words
-  def anagrams(word_one, word_two)
-    # code to go here
+  def anagram_check(word_one, word_two)
+    word_one_array = word_one.split("")
+    word_two_array = word_two.split("")
+    anagram = word_one_array.all? { |letter| word_two_array.include?(letter) }
+    if anagram
+      "#{word_one} and #{word_two} are anagrams"
+    else
+      "#{word_one} and #{word_two} are not anagrams"
+    end
   end
 end
--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -4,6 +4,6 @@ require 'anagrams_antigrams'
 describe('Words#anagrams') do
   it('will compare two words and return whether they are anagrams') do
     words = Words.new()
-    expect(words.anagrams('ruby', 'bury')).to(eq('ruby and bury are anagrams'))
+    expect(words.anagram_check('ruby', 'bury')).to(eq('ruby and bury are anagrams'))
   end
 end
